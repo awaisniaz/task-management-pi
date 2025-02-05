15b531b52951cfa6d7a288a0621500cce258c8d4
awaisniaz <awaisniaz1995@gmail.com>
2025-02-05 22:48:20 +0500

    fix: add constructor to initialize

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
+  constructor(){}
   async registerUser():Promise<any> {}
   async login():Promise<any> {}
   async getDetails():Promise<any>{}
